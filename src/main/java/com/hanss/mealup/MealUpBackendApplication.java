76a6ac701b9ab779d3321e1e7003f13cfb5d45f7
pavel.grudanov <pavel.grudanov@ocrv.ru>
2021-03-15 19:18:28 +0300

    Initial commit

---
+++ src/main/java/com/hanss/mealup/MealUpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hanss.mealup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealUpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealUpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanss/mealup/MealUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanss.mealup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealUpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
