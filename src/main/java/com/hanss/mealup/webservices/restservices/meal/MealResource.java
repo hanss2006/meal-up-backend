fd2a440bda214f85ae95e20a7368d97fcf8d2682
pavel.grudanov <pavel.grudanov@ocrv.ru>
2021-03-20 22:23:35 +0300

    Initial commit

--- src/main/java/com/hanss/mealup/webservices/restservices/meal/MealResource.java
+++ src/main/java/com/hanss/mealup/webservices/restservices/meal/MealResource.java
@@ -1,6 +1,7 @@
 package com.hanss.mealup.webservices.restservices.meal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class MealResource {
     @Autowired
     private MealHardcodedService mealService;
